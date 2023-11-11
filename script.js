3837e0356cd316b496afd05de4af340571a678e9
Adhvik Krishna <adhvik.krishna@gmail.com>
2023-11-11 15:53:01 -0800

    FIxed into single file

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-<div>Teachable Machine Image Model</div>
-<button type="button" onclick="init()">Start</button>
-<div id="webcam-container"></div>
-<div id="label-container"></div>
-<script src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@latest/dist/tf.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@teachablemachine/image@latest/dist/teachablemachine-image.min.js"></script>
-<script type="text/javascript">
     // More API functions here:
     // https://github.com/googlecreativelab/teachablemachine-community/tree/master/libraries/image
 
@@ -56,5 +49,3 @@
             labelContainer.childNodes[i].innerHTML = classPrediction;
         }
     }
-</script>
-
